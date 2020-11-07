b40207387f12c760f1297fe2b149ae5e9cacaa7a
Leonard Scheidemantel <leonard.scheidemantel@googlemail.com>
2020-11-07 13:13:02 +0100

    Format code

--- src/index.ts
+++ src/index.ts
@@ -12,21 +12,23 @@ import { JsonRpc } from "eosjs";
  *  - Initialization of interval based validation
  */
 function main() {
-  console.log(" _    __      ___     __      __  _                                          ___ \n" +
-    "| |  / /___ _/ (_)___/ /___ _/ /_(_)___  ____  _________  ________     _   _|__ \\\n" +
-    "| | / / __ `/ / / __  / __ `/ __/ / __ \\/ __ \\/ ___/ __ \\/ ___/ _ \\   | | / /_/ /\n" +
-    "| |/ / /_/ / / / /_/ / /_/ / /_/ / /_/ / / / / /__/ /_/ / /  /  __/   | |/ / __/ \n" +
-    "|___/\\__,_/_/_/\\__,_/\\__,_/\\__/_/\\____/_/ /_/\\___/\\____/_/   \\___/    |___/____/")
-  console.log("    by Blacklusion - 2020\n\n")
+  console.log(
+    " _    __      ___     __      __  _                                          ___ \n" +
+      "| |  / /___ _/ (_)___/ /___ _/ /_(_)___  ____  _________  ________     _   _|__ \\\n" +
+      "| | / / __ `/ / / __  / __ `/ __/ / __ \\/ __ \\/ ___/ __ \\/ ___/ _ \\   | | / /_/ /\n" +
+      "| |/ / /_/ / / / /_/ / /_/ / /_/ / /_/ / / / / /__/ /_/ / /  /  __/   | |/ / __/ \n" +
+      "|___/\\__,_/_/_/\\__,_/\\__,_/\\__/_/\\____/_/ /_/\\___/\\____/_/   \\___/    |___/____/"
+  );
+  console.log("    by Blacklusion - 2020\n\n");
 
   logger.info("Starting up " + config.get("general.name") + "...");
 
   // Check if config file with all necessary settings exists
   if (!checkConfig()) {
-    logger.fatal("Not all settings were set. Aborting startup...")
+    logger.fatal("Not all settings were set. Aborting startup...");
     return;
   } else {
-    logger.info("Valid config/local.toml was found!")
+    logger.info("Valid config/local.toml was found!");
   }
 
   // Check if Pager mode is enabled
@@ -37,7 +39,7 @@ function main() {
     .then(async () => {
       logger.info("Successfully connected to database ");
 
-      logger.info("++++++++  STARTUP COMPLETE  ++++++++\n\n")
+      logger.info("++++++++  STARTUP COMPLETE  ++++++++\n\n");
 
       /**
        * Initialize interval based validations
@@ -217,13 +219,15 @@ function checkConfig(): boolean {
     ["validation.history_transaction_offset", "number"],
     ["validation.history_actions_block_time_delta", "number"],
     ["validation.hyperion_query_time_ms", "number"],
-    ["validation.social_services", "array"]
+    ["validation.social_services", "array"],
   ];
 
-
-  settings.forEach(setting => {
+  settings.forEach((setting) => {
     try {
-      if ((setting[1] === "array" && !Array.isArray(config.get(setting[0]))) || (setting[1] !== "array" && !(typeof config.get(setting[0]) === setting[1]))) {
+      if (
+        (setting[1] === "array" && !Array.isArray(config.get(setting[0]))) ||
+        (setting[1] !== "array" && !(typeof config.get(setting[0]) === setting[1]))
+      ) {
         logger.error(setting[0] + " was provided. But it is not of type " + setting[1]);
         allVariablesSet = false;
       }
@@ -231,7 +235,7 @@ function checkConfig(): boolean {
       logger.error(setting[0] + " was not provided!");
       allVariablesSet = false;
     }
-  })
+  });
 
   return allVariablesSet;
 }
